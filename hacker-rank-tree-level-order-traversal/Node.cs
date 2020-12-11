c21fba171ffdfdb9d15e4ba60ac4180a8299eeaa
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-12-11 01:20:25 -0300

    Solution

---
+++ hacker-rank-tree-level-order-traversal/Node.cs
@@ -0,0 +1,39 @@
+﻿namespace hacker_rank_tree_level_order_traversal
+{
+    public class Node
+    {
+        public Node(int data)
+        {
+            Data = data;
+        }
+
+        public Node Left
+        {
+            get;
+            private set;
+        }
+
+        public Node Right
+        {
+            get;
+            private set;
+        }
+
+        public int Data
+        {
+            get;
+            private set;
+        }
+
+
+        public void SetRightNode(Node node)
+        {
+            Right = node;
+        }
+
+        public void SetLeftNode(Node node)
+        {
+            Left = node;
+        }
+    }
+}
--- hacker-rank-tree-level-order-traversal/Program.cs
+++ hacker-rank-tree-level-order-traversal/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace hacker_rank_tree_level_order_traversal
 {
@@ -6,7 +8,64 @@ namespace hacker_rank_tree_level_order_traversal
     {
         static void Main()
         {
-            
+            Console.Write("Input the number of nodes: ");
+            int numberOfNodes = int.Parse(Console.ReadLine());
+            Node root = null;
+            Console.WriteLine("Input the nodes: ");
+            while (numberOfNodes-- > 0)
+            {
+                int data = int.Parse(Console.ReadLine());
+                root = Insert(root, data);
+            }
+            LevelOrder(root);
+            Console.ReadKey();
+        }
+
+        private static Node Insert(Node root, int data)
+        {
+            if (root is null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node currentNode;
+                if (data < root.Data)
+                {
+                    currentNode = Insert(root.Left, data);
+                    root.SetLeftNode(currentNode);
+                }
+                else
+                {
+                    currentNode = Insert(root.Right, data);
+                    root.SetRightNode(currentNode);
+                }
+                return root;
+            }
+        }
+
+        private static void LevelOrder(Node root)
+        {
+            if (root is null)
+            {
+                return;
+            }
+
+            List<Node> nodes = new List<Node>
+            {
+                root
+            };
+
+            while (nodes.Count > 0)
+            {
+                Node currentNode = nodes.FirstOrDefault();
+                nodes.Remove(currentNode);
+
+                Console.Write($"{currentNode.Data} ");
+                if (currentNode.Left is not null) nodes.Add(currentNode.Left);
+                if (currentNode.Right is not null) nodes.Add(currentNode.Right);
+            }
+
         }
     }
 }
